732db0e05c3cece64e1f784bfcbe18d3203d9a3b
Ali Nugen <alexandra.nugen@gmail.com>
2022-10-23 14:44:03 -0700

    updating database js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// A PUT method that accepts content and PUTS it into the DB
+export const putDb = async (id, content) => {
+  console.log('PUT to DB');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, content: content });
+  const result = await request;
+  console.log('Data has been saved to the database!', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// A GET method that GETS all the content from the DB
+export const getDb = async () => {
+  console.log('GET all data from DB');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
+// Initialize the DB
 initdb();
