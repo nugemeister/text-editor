91caff5b056c1f754019bf8f685dab7cbc1e9a21
Ali Nugen <alexandra.nugen@gmail.com>
2022-10-23 13:52:31 -0700

    updating webpack config using miniproject and day 3 activities from module 19

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,74 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: .
+// TODO: 
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    
+    // Adding and configuring workbox plugins for a service worker and manifest file
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './indez.html',
+        title: 'JATE Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE: Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Just Another Text Editor',
+        background_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    // Adding CSS loaders and babel to webpack
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/presest-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
   };
